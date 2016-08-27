fd1b4cb40e31b6d63c3f0e8d89b9aed5d3bacad7
ljzjohnson <1464811863@qq.com>
2016-08-27 10:02:29 +0800

    data mining and machine learning

---
+++ src/DecisionTree/DTInstances/ID3.java
@@ -0,0 +1,22 @@
+/*   
+ * Copyright (c) 2010-2020 Founder LZG. All Rights Reserved.   
+ *   
+ * This software is the confidential and proprietary information of   
+ * Founder. You shall not disclose such Confidential Information   
+ * and shall use it only in accordance with the terms of the agreements   
+ * you entered into with Founder.   
+ *   
+ */
+package DecisionTree.DTInstances;
+
+import DecisionTree.DecisionTree;
+
+/**
+ * description
+ * 
+ * @author luzhongguo
+ * @version 1.0, 2016年7月23日
+ */
+public class ID3 extends DecisionTree {
+
+}
